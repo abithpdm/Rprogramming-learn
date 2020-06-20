6fc61a9e953212d94e1ae518c56dc6a41b3b2408
Abith <abithpdm@gmail.com>
2020-06-21 00:06:13 +0530

    included actual functioning code (for loop)

--- Kickstart_2020_set_A.R
+++ Kickstart_2020_set_A.R
@@ -2,9 +2,23 @@
 testcases<-as.integer(readline())
 while (testcases>0)
 {
-  constraints<-strsplit(readline()," ")[[1]]
-  prices<-strsplit(readline()," ")[[1]]
+  constraints<-as.integer(strsplit(readline()," ")[[1]])
+  prices<-as.integer(strsplit(readline()," ")[[1]])
   sortedprice <-sort(prices)
-  
-  
+  count<-0
+  balance<-constraints[2]
+  for(i in as.integer(length(prices)))
+  {
+    if(sortedprice[i]<=balance)
+      {count<-count+1
+      balance<-balance-sortedprice[i]}
+    else
+      {break}
+  }
+  print(count)
+    
 }
+
+  
+  
+
