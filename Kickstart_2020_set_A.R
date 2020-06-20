363371d2ba4d041765e154eb13c151153b871c6e
Abith <abithpdm@gmail.com>
2020-06-20 23:49:25 +0530

    removed print statement to check whether allocation is working or not , its working fine , and the problem was i opened the file in interactive mode which is not supported for input ,opened the file in Rscrip mode

--- Kickstart_2020_set_A.R
+++ Kickstart_2020_set_A.R
@@ -1,13 +1,10 @@
 #program for google kick start 2020 set A (House allocation problem)
 testcases<-as.integer(readline())
-while (0<testcases)
+while (testcases>0)
 {
   constraints<-strsplit(readline()," ")[[1]]
   prices<-strsplit(readline()," ")[[1]]
   sortedprice <-sort(prices)
-  print(testcases)
-  print(constraints)
-  print(prices)
-  testcases<-testcases-1
+  
   
 }
