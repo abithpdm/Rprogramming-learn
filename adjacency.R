7a42f2b231f6a4e1fc5426fb9a27efcf75f4bee7
abithpdm <abithpdm@gmail.com>
2020-07-03 20:07:23 +0530

    doc

--- adjacency.R
+++ adjacency.R
@@ -4,8 +4,8 @@ for (i in testcase)
 
    {
      initiator <-1
-  string <-as.character(readline())
-  splittedstring<-strsplit(string,"")
+     string <-as.character(readline())
+     splittedstring<-strsplit(string,"")
   while(initiator!=length(string)){
    if(splittedstring[[initiator]]==splittedstring[[initiator+1]])
    {
