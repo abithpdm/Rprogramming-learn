0fe2c09160703bfe70ca99513988afa9459c56ac
abithpdm <abithpdm@gmail.com>
2020-07-10 19:40:15 +0530

    hdf5

--- ucsc_db.R
+++ ucsc_db.R
@@ -17,3 +17,17 @@ query <- dbSendQuery(hg19,"select * from affyU133Plus2 where misMatches between 
 #the remotely stored data is the fetched in  query
 # dbclear(<queryname>) to clear the remote query from the server
 affyMis <- fetch(query); quantile(affyMis$misMatches)
+
+#load rhdf5
+#to create hdf5 file
+created=h5createFile("example.h5")
+
+#create file within file
+
+created =h5createGroup("example.h5","foo")
+
+A=matrix(1:10,nr=5,nc=2)
+h5write(A,"example.h5","foo/A")
+B=array(seq(0.1,2.0,by=0.1),dim = c(5,2,2))
+attr(B,"scale")<-"liter"
+
